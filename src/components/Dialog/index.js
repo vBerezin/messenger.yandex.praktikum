ff2fbbe100f5c36d06d464fc93432d9eba228c86
v.berezin <v.berezin@corp.mail.ru>
2021-05-30 20:58:56 +0300

    [sprint-1]

--- src/components/Dialog/index.js
+++ src/components/Dialog/index.js
@@ -1,13 +1,16 @@
 import './style.scss';
 import template from './template.pug';
 import { Component } from '~common/scripts/utils/component';
+import { formSubmitHandler } from '~common/scripts/utils/form-submit-handler';
 import '~components/Message';
 
 export class Dialog extends Component {
   constructor(props) {
     super({ template, props });
   }
+
   render(container) {
     super.render(container);
+    this.el.addEventListener('submit', formSubmitHandler);
   }
 }
