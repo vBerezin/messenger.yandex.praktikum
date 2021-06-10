d9e3dc70b72c2edeb7a7deec33f1f1ef4882db56
v.berezin <v.berezin@corp.mail.ru>
2021-06-10 20:31:50 +0300

    [sprint-1]

--- server/app.js
+++ server/app.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 const PORT = 3000;
 
-app.use(express.static('./static'));
-
+app.use(express.static('./dist'));
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve('./dist/index.html'));
+});
 app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on http://localhost:${PORT}`);
 });
