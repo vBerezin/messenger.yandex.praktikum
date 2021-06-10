ff5119f8b0ff32012b17f137befaaf3b91e6d6ff
v.berezin <v.berezin@corp.mail.ru>
2021-06-10 21:40:26 +0300

    Revert "[sprint-1]"
    This reverts commit 58811a7c

--- src/common/scripts/modules/Component.js
+++ src/common/scripts/modules/Component.js
@@ -1,5 +1,5 @@
 import { EventEmitter } from 'events';
-import { Templator } from './Templator';
+import { Templator } from '~common/scripts/modules/Templator';
 
 export class Component {
   #state;
