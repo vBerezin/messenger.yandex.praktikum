8d95a418298fa9ec4211e20140ac95faf8cbbcc1
v.berezin <v.berezin@corp.mail.ru>
2021-06-11 12:23:24 +0300

    [sprint-1] bugs

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { Router } from '~common/scripts/modules/Router';
 import { ROUTES } from '~common/scripts/routes';
 import { page500 } from '~pages/page500';
 import { page404 } from '~pages/page404';
-import { pageSignIn } from '~pages/pageSignin';
+import { pageSignIn } from '~pages/pageSignIn';
 import { pageSignUp } from '~pages/pageSignUp';
 import { pageProfile } from '~pages/pageProfile';
 import { pageMessenger } from '~pages/pageMessenger';
