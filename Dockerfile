30245ae2f2f891da0e4c3a559e6d8c9fe875a717
v.berezin <v.berezin@corp.mail.ru>
2021-10-24 21:20:04 +0300

    [sprint-4]

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,9 @@
 FROM node:latest
 WORKDIR /usr/src/app
 
-COPY package*.json ./
 COPY . .
 
 RUN npm install
 RUN npm run build-prod
 
-CMD npm run serverdovk
+CMD ["node", "server.js"]
--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const path = require('path');
 const paths = require('./config/paths');
 
 const PORT = process.env.PORT || 3000;
-const HOSTNAME = '0.0.0.0';
 const BUILD_PATH = path.resolve(paths.build);
 const server = express();
 
@@ -11,6 +10,6 @@ server.use(express.static(BUILD_PATH));
 server.get('*', (req, res) => {
   res.sendfile(path.join(__dirname, BUILD_PATH, 'index.html'));
 });
-server.listen(PORT, HOSTNAME, () => {
-  console.info(`Server running on http://localhost:${PORT}`);
+server.listen(PORT, () => {
+  console.info(`Server running on port: ${PORT}`);
 });
